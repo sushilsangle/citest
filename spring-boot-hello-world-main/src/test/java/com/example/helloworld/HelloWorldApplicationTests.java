a9b018a85d7f270b26c563b255d5d9c0e970bbef
Sushil <sushilsangle.ss@gmail.com>
2024-08-25 16:37:03 +0530

    first commit

---
+++ spring-boot-hello-world-main/src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import com.example.helloworld.controller.HelloWorldController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+  
+}
